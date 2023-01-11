7d9557c82c758294df7774d87d36a667d4292eab
ahmidev <ahmid.aitouali@gmail>
2023-01-11 15:34:58 +0100

    add routerLinkActive

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
-import { SignUpComponent } from './sign-up/sign-up.component';
 
+
+import { SignUpComponent } from './sign-up/sign-up.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 
 const ROUTES: Routes = [
